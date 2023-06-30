f43e6aaa30f46e599175c8e50abdbd5339a302a1
vatsal2809 <99423464+vatsal2809@users.noreply.github.com>
2023-06-30 12:55:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React-1
         </a>
         {url && <p>Backend connected to url : {url}</p>}
       </header>
